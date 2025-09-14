4c2f41185656f479638baf03afd0d861f4a35a21
Sweety Barua <you@example.com baruahsweety08@gmail.com>
2025-09-14 12:23:30 +0600

    changed the theme

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,7 +11,7 @@ export const Home = () => {
   return (
     <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
       {/* Background Effects */}
-      <StarBackground />
+      {/* <StarBackground /> */}
 
       {/* Navbar */}
       <Navbar />
